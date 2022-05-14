17fd335b84afcc93cd47d918545667db9e264f79
khosrora <khosrora153333@gmail.com>
2022-05-14 18:07:27 +0430

    [crete models] ==>  refactor

--- app/models/user.js
+++ app/models/user.js
@@ -6,15 +6,15 @@ const schema = mongoose.Schema({
 
     first_name: { type: String },
     last_name: { type: String },
-    username: { type: String },
-    email: { type: String },
+    username: { type: String, required: true, lowercase: true },
+    email: { type: String, lowercase: true },
     password: { type: String },
     phone: { type: String },
     otp: { type: Object, default: { code: 0, exires: 0 } },
     bills: { type: [], default: [] },
     discount: { type: Number, default: 0 },
     birthday: { type: String },
-    roles: { type: [String], default : ["USER"]}
+    roles: { type: [String], default: ["USER"] }
 
 })
 
