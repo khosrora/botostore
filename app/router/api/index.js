786e55dfa1ebcda8c8708acc9ba5f73c9899f59e
khosrora <khosrora153333@gmail.com>
2022-05-15 10:34:15 +0430

    [add swagger] ==> install swagger and use in project

--- app/router/api/index.js
+++ app/router/api/index.js
@@ -3,8 +3,21 @@ const homeController = require('../../http/controllers/api/home.controller');
 const router = require('express').Router();
 
 
+/**
+ * @swagger
+ * http://localhost:5000/:
+ *  get:
+ *      symmery: index of routes
+ *      tags: [indexPage]
+ *      description: get all need data for index page
+ *      responses: 
+ *          200:
+ *              description: success
+ *          404: 
+ *              description: notFound
+ */
 
-router.post("/", homeController.indexPage);
+router.get("/", homeController.indexPage);
 
 
 module.exports = { homeRoutes: router };
--- app/server.js
+++ app/server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const path = require('path');
 const createError = require('http-errors');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 const { AllRoutes } = require('./router/router');
 
 module.exports = class Application {
@@ -25,6 +27,25 @@ module.exports = class Application {
         this.#app.use(express.json());
         this.#app.use(express.urlencoded({ extended: true }));
         this.#app.use(express.static(path.join(__dirname, "..", "public")));
+        this.#app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc({
+            swaggerDefinition: {
+                info: {
+                    title: "boto Store",
+                    version: "1.0.0" , 
+                    description : "website for sell course and learn",
+                    contact : {
+                        name : "khosroRA" ,
+                        email : "khosrora153333@gmail.com"
+                    }
+                },
+                servers :[
+                    {
+                        url : "http://localhost:5000"
+                    }
+                ]
+            },
+            apis :["./app/router/**/*.js"]
+        })))
     }
     createServer() {
         const http = require('http');
